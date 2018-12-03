4e9005d00d13ceb2b7c1e78dec838f89c086a5b2
Peter Ahrens <Peter@30-9-110.wireless.csail.mit.edu>
2018-12-03 14:23:56 -0500

    not the greatest idea

--- src/CompiledStyles.jl
+++
@@ -1,9 +0,0 @@
-using Base.Broadcast: BroadcastStyle
-
-struct CompiledStyle{Style<:BroadcastStyle} <: BroadcastStyle
-    style::Style
-end
-
-copyto_thunk(dest, desttype, src, srctype)
-
-getindex_thunk(arr, arrtype, idx, idxtype)
