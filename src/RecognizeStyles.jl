33c24cdc254366c24be8b5c3c801a47ebd12f520
Peter Ahrens <ptrahrens@gmail.com>
2019-02-21 17:33:48 -0500

    pseudocode!

--- src/RecognizeStyles.jl
+++ src/RecognizeStyles.jl
@@ -32,6 +32,7 @@ end
 
 
 
+#=
 """
     simplify(arr)
 
@@ -45,3 +46,36 @@ Apply global rules to simplify the array expression `arr`.
         $simple_expr
     end
 end
+
+"""
+    match(arr)
+
+Apply global rules to find best copyto implementation for expression `arr`.
+"""
+@generated function match(arr)
+    simple_expr = Rewrite.with_context(RecognizeStyles.CONTEXT)) do
+        normalize(@term(reprexpr(:arr, arr)), RecognizeStyles.RULES)
+    end
+    for kernel in RecognizeStyles.KERNELS
+        match(kernel, simple_expr)
+        return quote
+            $kernel
+        end
+    end
+end
+
+struct RecognizeStyle <: BroadcastStyle end
+
+
+function Base.copy(arr::Broadcasted{RecognizeStyle})
+    lookup(simplify(arr))
+end
+
+function lookup(arr::Broadcasted{+, Tuple{Array{Int}, Array{Int}}})
+    A = arr.args[1]
+    B = arr.args[2]
+    for i in eachindex(A)
+
+    end
+end
+=#
