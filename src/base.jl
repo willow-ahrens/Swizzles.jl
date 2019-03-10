7b23640693163904d97f1aded8514676e5546a53
Peter Ahrens <ptrahrens@gmail.com>
2019-03-09 21:47:53 -0500

    didn't need

--- src/base.jl
+++ src/base.jl
@@ -30,6 +30,7 @@ module BaseHacks
         Base._range(st, nothing, nothing, length(s)) #gotta use this version of range instead of const-propping through the kwarg version.
     end
 
+    #=
     Base.@propagate_inbounds function Base.getindex(iter::CartesianIndices{N,<:NTuple{N,Base.OneTo}}, I::Vararg{Int, N}) where {N}
         @boundscheck checkbounds(iter, I...)
         CartesianIndex(I)
@@ -39,6 +40,7 @@ module BaseHacks
         @boundscheck checkbounds(iter, I...)
         CartesianIndex(I .- first.(Base.axes1.(iter.indices)) .+ first.(iter.indices))
     end
+    =#
 
 
     @info "BaseHacks is online. all your Base are belong to us."
