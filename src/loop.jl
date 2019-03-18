c879e424c2b3e3a298e922b8e6de7697838ce2b9
Peter Ahrens <ptrahrens@gmail.com>
2019-03-18 17:42:10 -0400

    now we are fast

--- src/loop.jl
+++ src/loop.jl
@@ -4,3 +4,19 @@ Base.@propagate_inbounds function loop(f::F, itr) where {F}
     end
     return nothing
 end
+
+@generated function loop(f::F, itr::CartesianIndices{N}) where {F, N}
+    thunk = :(f(CartesianIndex($([Symbol("i$n") for n in 1:N]...))))
+    for n in 1:N
+        thunk = quote
+            for $(Symbol("i$n")) = itr.indices[$n]
+                $thunk
+            end
+        end
+    end
+    return quote
+        Base.@_propagate_inbounds_meta
+        $thunk
+        return nothing
+    end
+end
