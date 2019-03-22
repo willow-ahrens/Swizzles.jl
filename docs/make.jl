f09e4301b82d72912331ab698ce5f37afbafae79
Peter Ahrens <ptrahrens@gmail.com>
2019-03-22 18:23:29 -0400

    make docs build

--- docs/make.jl
+++ docs/make.jl
@@ -3,14 +3,15 @@ using Swizzles
 
 makedocs(
     sitename="Swizzles.jl",
-    modules = [Swizzles]
-    authors = "Peter Ahrens"
+    modules = [Swizzles],
+    authors = "Peter Ahrens",
     pages = [
         "Home" => "index.md"
-        "Getting Started" => "getting_started.md"
-        "Swizzle Friends" => "swizzle_friends.md"
-        "Abstracter Arrays" => "abstracter_arrays.md"
-        "API" => "api.md"
+        #"Getting Started" => "getting_started.md"
+        #"Swizzle Friends" => "swizzle_friends.md"
+        #"Abstracter Arrays" => "abstracter_arrays.md"
+        #"API" => "api.md"
+    ]
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
