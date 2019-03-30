1f36bc2531f1a4c41daddc7d26ca381e13d1541f
Peter Ahrens <ptrahrens@gmail.com>
2019-03-30 00:16:34 -0400

    gud

--- test/EachindexArrays.jl
+++ test/EachindexArrays.jl
@@ -3,12 +3,8 @@ using Swizzles.EachindexArrays
 
 @testset "EachindexArrays" begin
     I = CartesianIndices((1:5, 1:5))
-    A = copy(I)
     tape = []
     casette(_, i) = push!(tape, i)
-    Swizzle(casette).(nothing, I)
-    #println(Focus(2, 1).(tape))
-    tape = []
     Swizzle(casette).(nothing, Tile(2,2).(I))
-    println(Focus(2, 1).(tape))
+    @test tape == CartesianIndex{2}[CartesianIndex(1, 1) CartesianIndex(2, 1) CartesianIndex(1, 2) CartesianIndex(2, 2) CartesianIndex(3, 1) CartesianIndex(4, 1) CartesianIndex(3, 2) CartesianIndex(4, 2) CartesianIndex(5, 1) CartesianIndex(5, 2) CartesianIndex(1, 3) CartesianIndex(2, 3) CartesianIndex(1, 4) CartesianIndex(2, 4) CartesianIndex(3, 3) CartesianIndex(4, 3) CartesianIndex(3, 4) CartesianIndex(4, 4) CartesianIndex(5, 3) CartesianIndex(5, 4) CartesianIndex(1, 5) CartesianIndex(2, 5) CartesianIndex(3, 5) CartesianIndex(4, 5) CartesianIndex(5, 5)]
 end
