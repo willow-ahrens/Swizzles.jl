99e11d2e8c7d6e7be2e482d32b983ce0c52f5050
Peter Ahrens <ptrahrens@gmail.com>
2019-04-11 00:18:56 -0400

    yay

--- test/StylishArrays.jl
+++ test/StylishArrays.jl
@@ -31,31 +31,31 @@ using LinearAlgebra
         @test root(y + z) == Inf
     end
 
-    struct GenTest{T, N, Arr<:AbstractArray{T, N}} <: StylishArray{T, N}
+    struct TestStylishArray{T, N, Arr<:AbstractArray{T, N}} <: StylishArray{T, N}
         parent::Arr
     end
-    Base.size(arr::GenTest) = size(arr.parent)
-    Base.axes(arr::GenTest) = axes(arr.parent)
-    Base.getindex(arr::GenTest, i...) = arr.parent[i...]
-    Base.getindex(arr::GenTest, i::Int) = arr.parent[i]
-    Base.getindex(arr::GenTest, i::CartesianIndex) = arr.parent[i]
-    Base.setindex!(arr::GenTest, x, i...) = arr.parent[i...] = x
-    Base.setindex!(arr::GenTest, x, i::Int) = arr.parent[i] = x
-    Base.setindex!(arr::GenTest, x, i::CartesianIndex) = arr.parent[i] = x
+    Base.size(arr::TestStylishArray) = size(arr.parent)
+    Base.axes(arr::TestStylishArray) = axes(arr.parent)
+    Base.getindex(arr::TestStylishArray, i...) = arr.parent[i...]
+    Base.getindex(arr::TestStylishArray, i::Int) = arr.parent[i]
+    Base.getindex(arr::TestStylishArray, i::CartesianIndex) = arr.parent[i]
+    Base.setindex!(arr::TestStylishArray, x, i...) = arr.parent[i...] = x
+    Base.setindex!(arr::TestStylishArray, x, i::Int) = arr.parent[i] = x
+    Base.setindex!(arr::TestStylishArray, x, i::CartesianIndex) = arr.parent[i] = x
 
     @testset "Base" begin
 
         @testset "copyto!" begin
-            for A in ([1 2 3; 4 5 6; 7 8 9], GenTest([1 2 3; 4 5 6; 7 8 9]))
-                B = GenTest(zeros(3,3))
+            for A in ([1 2 3; 4 5 6; 7 8 9], TestStylishArray([1 2 3; 4 5 6; 7 8 9]))
+                B = TestStylishArray(zeros(3,3))
                 @test A == copyto!(B, A)
                 @test A == B
-                B = GenTest(zeros(9))
+                B = TestStylishArray(zeros(9))
                 #@test reshape(A, :) == copyto!(B, A) #FIXME
                 #@test reshape(A, :) == B #FIXME
             end
 
-            A = GenTest([1 2 3; 4 5 6; 7 8 9])
+            A = TestStylishArray([1 2 3; 4 5 6; 7 8 9])
             B = zeros(3,3)
             @test A == copyto!(B, A)
             @test A == B
@@ -66,94 +66,94 @@ using LinearAlgebra
 
         @testset "getindex and setindex!" begin
             A = [1 2 3; 4 5 6; 7 8 9]
-            @test GenTest(A)[1] == A[1]
-            @test GenTest(A)[1:3] == A[1:3]
-            @test GenTest(A)[2:3, 1:2] == A[2:3, 1:2]
-            @test GenTest(A)[2, 1:3] == A[2, 1:3]
+            @test TestStylishArray(A)[1] == A[1]
+            @test TestStylishArray(A)[1:3] == A[1:3]
+            @test TestStylishArray(A)[2:3, 1:2] == A[2:3, 1:2]
+            @test TestStylishArray(A)[2, 1:3] == A[2, 1:3]
 
             A = [1 2 3; 4 5 6; 7 8 9]
             B = [1 2 3; 4 5 6; 7 8 9]
-            GenTest(A)[1] = -1
+            TestStylishArray(A)[1] = -1
             B[1] = -1
             @test A == B
 
             A = [1 2 3; 4 5 6; 7 8 9]
             B = [1 2 3; 4 5 6; 7 8 9]
-            GenTest(A)[1:3] = [-1 -1 -1]
+            TestStylishArray(A)[1:3] = [-1 -1 -1]
             B[1:3] = [-1 -1 -1]
             @test A == B
 
             A = [1 2 3; 4 5 6; 7 8 9]
             B = [1 2 3; 4 5 6; 7 8 9]
-            GenTest(A)[2:3, 1:2] = [-1 -1; -1 -1]
+            TestStylishArray(A)[2:3, 1:2] = [-1 -1; -1 -1]
             B[2:3, 1:2] = [-1 -1; -1 -1]
             @test A == B
 
             A = [1 2 3; 4 5 6; 7 8 9]
             B = [1 2 3; 4 5 6; 7 8 9]
-            GenTest(A)[2, 1:3] = [-1 -1 -1]
+            TestStylishArray(A)[2, 1:3] = [-1 -1 -1]
             B[2, 1:3] = [-1 -1 -1]
             @test A == B
         end
 
         @testset "map" begin
             A = [1 2 3; 4 5 6; 7 8 9]
-            @test map(x -> x == 2, GenTest(A)) == map(x -> x == 2, A)
+            @test map(x -> x == 2, TestStylishArray(A)) == map(x -> x == 2, A)
 
             A = [1 2 3; 4 5 6; 7 8 9]
-            @test map((a, b) -> a + b == 4, GenTest(A), GenTest(A)) == map((a, b) -> a + b == 4, A, A)
+            @test map((a, b) -> a + b == 4, TestStylishArray(A), TestStylishArray(A)) == map((a, b) -> a + b == 4, A, A)
         end
 
         @testset "foreach" begin
             A = [1 2 3; 4 5 6; 7 8 9]
             B = [1 2 3; 4 5 6; 7 8 9]
             C = [1 2 3; 4 5 6; 7 8 9]
-            @test foreach(i -> B[i] = -i, GenTest(A)) == foreach(i -> C[i] = -i, A)
+            @test foreach(i -> B[i] = -i, TestStylishArray(A)) == foreach(i -> C[i] = -i, A)
             @test B == C
 
             A = [1 2 3; 4 5 6; 7 8 9]
             B = [1 2 3; 4 5 6; 7 8 9]
             C = [1 2 3; 4 5 6; 7 8 9]
-            @test foreach((a, b) -> B[a] = b, GenTest(A), -A) == foreach((a, b) -> C[a] = b, A, -A)
+            @test foreach((a, b) -> B[a] = b, TestStylishArray(A), -A) == foreach((a, b) -> C[a] = b, A, -A)
             @test B == C
 
             A = [1 2 3; 4 5 6; 7 8 9]
             B = [1 2 3; 4 5 6; 7 8 9]
             C = [1 2 3; 4 5 6; 7 8 9]
-            @test foreach((a, b) -> B[a] = b, GenTest(A), -A) == foreach((a, b) -> C[a] = b, A, -A)
+            @test foreach((a, b) -> B[a] = b, TestStylishArray(A), -A) == foreach((a, b) -> C[a] = b, A, -A)
             @test B == C
         end
 
         @testset "reductions" begin
             A = [1 2 3; 4 5 6; 7 8 9]
 
-            @test mapreduce(-, +, GenTest(A)) == mapreduce(-, +, A)
-            @test mapreduce(-, +, GenTest(A), dims=2) == mapreduce(-, +, A, dims=2)
-            @test mapreduce(-, +, GenTest(A), dims=(1, 2)) == mapreduce(-, +, A, dims=(1, 2))
-            @test mapreduce(-, +, GenTest(A), dims=:) == mapreduce(-, +, A, dims=:)
-            @test mapreduce(-, +, GenTest(A), dims=()) == mapreduce(-, +, A, dims=())
-            @test mapreduce(-, +, GenTest(A), init=10) == mapreduce(-, +, A, init=10)
+            @test mapreduce(-, +, TestStylishArray(A)) == mapreduce(-, +, A)
+            @test mapreduce(-, +, TestStylishArray(A), dims=2) == mapreduce(-, +, A, dims=2)
+            @test mapreduce(-, +, TestStylishArray(A), dims=(1, 2)) == mapreduce(-, +, A, dims=(1, 2))
+            @test mapreduce(-, +, TestStylishArray(A), dims=:) == mapreduce(-, +, A, dims=:)
+            @test mapreduce(-, +, TestStylishArray(A), dims=()) == mapreduce(-, +, A, dims=())
+            @test mapreduce(-, +, TestStylishArray(A), init=10) == mapreduce(-, +, A, init=10)
 
-            @test reduce(+, GenTest(A)) == reduce(+, A)
-            @test reduce(+, GenTest(A), dims=2) == reduce(+, A, dims=2)
-            @test reduce(+, GenTest(A), dims=(1, 2)) == reduce(+, A, dims=(1, 2))
-            @test reduce(+, GenTest(A), dims=:) == reduce(+, A, dims=:)
-            @test reduce(+, GenTest(A), dims=()) == reduce(+, A, dims=())
-            @test reduce(+, GenTest(A), init=10) == reduce(+, A, init=10)
+            @test reduce(+, TestStylishArray(A)) == reduce(+, A)
+            @test reduce(+, TestStylishArray(A), dims=2) == reduce(+, A, dims=2)
+            @test reduce(+, TestStylishArray(A), dims=(1, 2)) == reduce(+, A, dims=(1, 2))
+            @test reduce(+, TestStylishArray(A), dims=:) == reduce(+, A, dims=:)
+            @test reduce(+, TestStylishArray(A), dims=()) == reduce(+, A, dims=())
+            @test reduce(+, TestStylishArray(A), init=10) == reduce(+, A, init=10)
 
-            @test sum(GenTest(A)) == sum(A)
-            @test sum(GenTest(A), dims=2) == sum(A, dims=2)
-            @test sum(GenTest(A), dims=(1, 2)) == sum(A, dims=(1, 2))
-            @test sum(GenTest(A), dims=:) == sum(A, dims=:)
-            @test sum(GenTest(A), dims=()) == sum(A, dims=())
-            @test sum(GenTest(A), init=10) == reduce(+, A, init=10)
+            @test sum(TestStylishArray(A)) == sum(A)
+            @test sum(TestStylishArray(A), dims=2) == sum(A, dims=2)
+            @test sum(TestStylishArray(A), dims=(1, 2)) == sum(A, dims=(1, 2))
+            @test sum(TestStylishArray(A), dims=:) == sum(A, dims=:)
+            @test sum(TestStylishArray(A), dims=()) == sum(A, dims=())
+            @test sum(TestStylishArray(A), init=10) == reduce(+, A, init=10)
         end
     end
 
     @testset "LinearAlgebra" begin
         @testset "norm" begin
             for (X, Y) in ((rand(3, 4), rand(3,4)), (rand(3), rand(3)))
-                @test dot(GenTest(X), GenTest(Y)) ≈ dot(X, Y)
+                @test dot(TestStylishArray(X), TestStylishArray(Y)) ≈ dot(X, Y)
             end
 
             big = prevfloat(typemax(Float64))/16
@@ -161,7 +161,7 @@ using LinearAlgebra
 
             for ps in ((-Inf,), (-3.0,), (-1,), (-0.5,), (0,), (0.5,), (1,), (2,), (3.0,), (Inf), ())
                 for X in ([1, 2], [big, big], [small, small], rand(3, 3), rand(3))
-                    @test norm(GenTest(X), ps...) ≈ norm(X, ps...)
+                    @test norm(TestStylishArray(X), ps...) ≈ norm(X, ps...)
                 end
             end
         end
@@ -181,17 +181,17 @@ using LinearAlgebra
                 catch MethodError
                 end
                 if valid
-                    @test GenTest(a) * b ≈ a * b
-                    @test a * GenTest(b) ≈ a * b
-                    @test GenTest(a) * GenTest(b) ≈ a * b
+                    @test TestStylishArray(a) * b ≈ a * b
+                    @test a * TestStylishArray(b) ≈ a * b
+                    @test TestStylishArray(a) * TestStylishArray(b) ≈ a * b
 
-                    @test mul!(identity.(y), GenTest(a), b) ≈ mul!(identity.(y), a, b)
-                    @test mul!(identity.(y), a, GenTest(b)) ≈ mul!(identity.(y), a, b)
-                    @test mul!(identity.(y), GenTest(a), GenTest(b)) ≈ mul!(identity.(y), a, b)
-                    @test mul!(GenTest(identity.(y)), a, b) ≈ mul!(identity.(y), a, b)
-                    @test mul!(GenTest(identity.(y)), GenTest(a), b) ≈ mul!(identity.(y), a, b)
-                    @test mul!(GenTest(identity.(y)), a, GenTest(b)) ≈ mul!(identity.(y), a, b)
-                    @test mul!(GenTest(identity.(y)), GenTest(a), GenTest(b)) ≈ mul!(identity.(y), a, b)
+                    @test mul!(identity.(y), TestStylishArray(a), b) ≈ mul!(identity.(y), a, b)
+                    @test mul!(identity.(y), a, TestStylishArray(b)) ≈ mul!(identity.(y), a, b)
+                    @test mul!(identity.(y), TestStylishArray(a), TestStylishArray(b)) ≈ mul!(identity.(y), a, b)
+                    @test mul!(TestStylishArray(identity.(y)), a, b) ≈ mul!(identity.(y), a, b)
+                    @test mul!(TestStylishArray(identity.(y)), TestStylishArray(a), b) ≈ mul!(identity.(y), a, b)
+                    @test mul!(TestStylishArray(identity.(y)), a, TestStylishArray(b)) ≈ mul!(identity.(y), a, b)
+                    @test mul!(TestStylishArray(identity.(y)), TestStylishArray(a), TestStylishArray(b)) ≈ mul!(identity.(y), a, b)
                 end
             end
         end
@@ -203,10 +203,10 @@ using LinearAlgebra
             w = rand(Complex{Float64}, 3)
 
             for (a) in (A, u, v, w)
-                @test transpose(GenTest(a)) == transpose(a)
-                @test transpose(GenTest(a)) isa Swizzles.SwizzledArray
-                @test adjoint(GenTest(a)) == adjoint(a)
-                @test adjoint(GenTest(a)) isa Swizzles.SwizzledArray
+                @test transpose(TestStylishArray(a)) == transpose(a)
+                @test transpose(TestStylishArray(a)) isa Swizzles.SwizzledArray
+                @test adjoint(TestStylishArray(a)) == adjoint(a)
+                @test adjoint(TestStylishArray(a)) isa Swizzles.SwizzledArray
             end
         end
 
@@ -217,15 +217,15 @@ using LinearAlgebra
             w = rand(3)
 
             for a in (A, u, v, w)
-                @test permutedims(GenTest(a)) == permutedims(a)
-                @test permutedims(GenTest(a)) isa Swizzles.SwizzledArray
+                @test permutedims(TestStylishArray(a)) == permutedims(a)
+                @test permutedims(TestStylishArray(a)) isa Swizzles.SwizzledArray
             end
 
             A = rand(3,3,3)
 
             for perm in [(1, 2, 3), (1, 3, 2), (2, 1, 3), (2, 3, 1), (3, 1, 2), (3, 2, 1)]
-                @test permutedims(GenTest(A), perm) == permutedims(A, perm)
-                @test permutedims(GenTest(A), perm) isa Swizzles.SwizzledArray
+                @test permutedims(TestStylishArray(A), perm) == permutedims(A, perm)
+                @test permutedims(TestStylishArray(A), perm) isa Swizzles.SwizzledArray
             end
         end
     end
