de2372895c4688b09624e37ffd7dfe22bf70d7b2
Peter Ahrens <ptrahrens@gmail.com>
2019-04-15 01:35:32 -0400

    eltype should work too (probs lots of bugs for tony to find lololol)

--- test/SimplifyStyles.jl
+++ test/SimplifyStyles.jl
@@ -96,6 +96,7 @@ end
                   Swizzle(+)(@_(0 .+ [1, 2, 3] .+ [1 2 3; 4 5 6; 7 8 9])),
                   Swizzle(+, 1)(@_(0 .+ [1, 2, 3] .+ [1 2 3; 4 5 6; 7 8 9]))]
             @test kept.(keeps(veval(evaluable(rewriteable(:root, typeof(lift_keeps(x)))...)))) == kept.(keeps(x))
+            @test eltype(veval(evaluable(rewriteable(:root, typeof(x))...))) == eltype(x)
         end
     end
 end
